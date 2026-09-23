e92cf73fd1dd5ae63449502b4256eb4c76dea86c
RINADS <rinads.india@gmail.com>
2026-09-23 08:43:19 +0000

    refactor(projects): use UX v2 project intake shell

--- apps/website/app/projects/ProjectsClient.tsx
+++ apps/website/app/projects/ProjectsClient.tsx
@@ -1,12 +1,7 @@
 "use client";
 
-import "@/app/projects/projects.css";
 import { ProjectsLanding } from "@/components/projects/ProjectsLanding";
 
 export function ProjectsClient() {
-  return (
-    <main id="main">
-      <ProjectsLanding />
-    </main>
-  );
+  return <ProjectsLanding />;
 }
